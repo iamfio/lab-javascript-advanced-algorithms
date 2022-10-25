54e10a444ecb950bc403a8c6cb1b7baa3945ca28
Fiodor Gorobet <iamfiodorgo@gmail.com>
2022-10-25 09:59:29 +0200

    Solved Lab

--- src/queue/queue-data-structure.js
+++ src/queue/queue-data-structure.js
@@ -5,25 +5,33 @@ class Queue {
   }
 
   canEnqueue() {
-    // ... your code goes here
+    return this.queueControl.length ? false : true;
   }
 
   isEmpty() {
-    // ... your code goes here
+    return this.canEnqueue();
   }
 
   enqueue(item) {
-    // ... your code goes here
+    this.queueControl.push(item);
+    if (this.queueControl.length > this.MAX_SIZE) {
+      throw new Error("QUEUE_OVERFLOW");
+    }
+
+    return this.queueControl
   }
 
   dequeue() {
-    // ... your code goes here
+    if (this.queueControl.length === 0) {
+      throw new Error("QUEUE_UNDERFLOW");
+    }
+    return this.queueControl.shift();
   }
 
   display() {
-    // ... your code goes here
-  }  
+    return this.queueControl
+  }
 }
 
 // This is required to enable the automated tests, please ignore it.
-if (typeof module !== 'undefined') module.exports = Queue;
+if (typeof module !== "undefined") module.exports = Queue;
--- src/stack/stack-data-structure.js
+++ src/stack/stack-data-structure.js
@@ -5,25 +5,37 @@ class Stack {
   }
 
   canPush() {
-    // ... your code goes here
+    return this.stackControl.length ? false : true;
   }
 
   isEmpty() {
-    // ... your code goes here
+    if (!this.canPush()) return false;
+
+    return this.stackControl.length ? false : true;
   }
 
   push(item) {
-    // ... your code goes here
+    if (this.stackControl.length === this.MAX_SIZE) {
+      throw new Error("STACK_OVERFLOW");
+    }
+
+    this.stackControl.push(item);
+
+    return this.stackControl;
   }
 
   pop() {
-    // ... your code goes here
+    if (!this.stackControl.length) {
+      throw new Error("STACK_UNDERFLOW");
+    }
+
+    return this.stackControl.pop();
   }
 
   display() {
-    // ... your code goes here
-  }  
+    return this.stackControl;
+  }
 }
 
 // This is required to enable the automated tests, please ignore it.
-if (typeof module !== 'undefined') module.exports = Stack;
+if (typeof module !== "undefined") module.exports = Stack;
